2f38b7db68c2b3b872163894fb58953e8260ac6b
Abdus Subhan <itssubhan_m@yahoo.com>
2025-04-05 13:36:08 +0500

    FACEBOOK Clone

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -11,19 +11,25 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -34,8 +40,11 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 
 import androidx.compose.material3.Surface
@@ -61,7 +70,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -83,32 +95,102 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FirstApp()
+            FacebookLoginPage()
         }
     }
 }
 
 @Composable
-fun FirstApp() {
-    var name by remember { mutableStateOf("") }
-    val context = LocalContext.current
+fun FacebookLoginPage() {
+    var userName by remember {
+        mutableStateOf("")
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0XFF12222e)), contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .clip(shape = RoundedCornerShape(200.dp))
+                    .size(85.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.facebook),
+                    contentDescription = "Facebook logo",
+                    modifier = Modifier.size(80.dp),
+                    colorFilter = ColorFilter.tint(Color(0XFF1677ef))
+
+                )
+            }
+            Spacer(modifier = Modifier.height(70.dp))
+            TextField(
+                value = userName,
+                onValueChange = { userName = it },
+                modifier = Modifier.width(320.dp),
+                placeholder = { Text(text = "Mobile number or email address") })
+            Spacer(modifier = Modifier.height(10.dp))
+            TextField(
+                value = userName,
+                onValueChange = { userName = it },
+                modifier = Modifier.width(320.dp),
+                placeholder = { Text(text = "Password") })
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.width(320.dp),
+                colors = ButtonDefaults.buttonColors(Color(0XFF1677ef))
+            ) {
+                Text(text = "Log In", fontSize = 16.sp)
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Forgotten Password?",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.width(320.dp)
+            )
+            Spacer(modifier = Modifier.height(150.dp))
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier.width(320.dp),
+                shape = RoundedCornerShape(50.dp),
+                border = BorderStroke(2.dp, color = Color(0xFF1677EF)),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0XFF1677ef))
+            ) { Text(text = "Create new account") }
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                modifier = Modifier.width(140.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    modifier = Modifier.size(40.dp),
+                    colorFilter = ColorFilter.tint(Color(0XFFc1d0d8)),
+                    painter = painterResource(id = R.drawable.meta),
+                    contentDescription = "Meta icon"
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "Meta", color = Color(0XFFc1d0d8), fontSize = 20.sp)
+
+            }
 
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text(text = "Enter Your Name") }
-        )
+        }
+
 
     }
-
 
 }
 
 
-@Preview(showBackground = true, name = "Login Page")
+@Preview(showBackground = true)
 @Composable
 fun FirstAppPreview() {
-    FirstApp()
+    FacebookLoginPage()
 }
